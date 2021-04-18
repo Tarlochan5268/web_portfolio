5132c65c4828a0eac49d4d820ac714051050b4b7
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2021-04-17 23:46:32 -0400

    added images

--- lib/Widgets/avatarwidget.dart
+++ lib/Widgets/avatarwidget.dart
@@ -21,7 +21,7 @@ class AvatarWidget extends StatelessWidget {
           ),
           child: CircleAvatar(
             backgroundColor: Colors.transparent,
-            backgroundImage: NetworkImage('images/profile.png'),
+            backgroundImage: NetworkImage('assets/assets/images/profile.png'),
             /*backgroundImage: NetworkImage(
               "https://cors.bridged.cc/https://drive.google.com/uc?export=view&id=1a3zTT8nBhBWxgaHL6XfDWlakyH1K-apU",
             ),*/
